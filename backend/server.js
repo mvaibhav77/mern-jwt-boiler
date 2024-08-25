1107f4d32d9414525b45f729579fa943bffe53aa
Vaibhav Shukla <shukla.vaibhav1077@gmail.com>
2024-08-25 17:31:25 +0530

    feat: deploy ready

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 import express from "express";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
-dotenv.config(); 
+dotenv.config();
 import userRoutes from "./routes/userRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 const port = process.env.PORT || 5000;
 
@@ -16,14 +17,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => {
-  res.send("SERVER IS READY");
-});
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("SERVER IS READY");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
-
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
