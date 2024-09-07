15f736f865397dc2d0e78602239d534c2127da9a
Vaibhav Shukla <shukla.vaibhav1077@gmail.com>
2024-09-07 22:31:33 +0530

    feat: cors error fixed

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import connectDB from "./config/db.js";
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 import userRoutes from "./routes/userRoutes.js";
@@ -12,6 +13,8 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
